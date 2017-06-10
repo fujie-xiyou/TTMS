33d0a01169bfbb63465e3b696fc49346f981c6c7
付杰蜀黍 <fujie.me@qq.com>
2017-06-10 17:25:24 +0800

    坐等模板,不知道该做什么了....

---
+++ Persistence/Studio_Persist.h
@@ -0,0 +1,11 @@
+/*************************************************************************
+	>    File Name: Studio_Persist.h
+	>       Author: fujie
+	>         Mail: fujie.me@qq.com
+	> Created Time: 2017年06月10日 星期六 17时23分31秒
+ ************************************************************************/
+
+#ifndef _STUDIO_PERSIST_H
+#define _STUDIO_PERSIST_H
+#endif
+int Studio_Perst_Insert(const studio_t *data);
