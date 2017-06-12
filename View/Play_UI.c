51d23bfb1e9cec0d1993ac75dfa5b8a5a7936f6d
hqqqqq <hqqqqq0613.cn>
2017-06-12 23:23:13 +0800

    剧目管理界面

--- View/Play_UI.c
+++ View/Play_UI.c
@@ -27,6 +27,13 @@
 	 * Return:      返回查找到的记录数目
 	 */
 void Play_UI_ShowList(play_list_t list, Pagination_t paging) {
+	printf("***************剧目信息********************\n");
+	printf("ID\t名称\t类型\t出品地区\t等级\t时长\t开始时间\t结束时间\t票价\n");
+	while(list) 
+	{
+		printf();
+		list=list->next;
+	}
 
 }
 
@@ -39,6 +46,82 @@ void Play_UI_ShowList(play_list_t list, Pagination_t paging) {
  * Return:      无
  */
 void Play_UI_MgtEntry(int flag){
+	int i, id;
+	char choice;
+
+	play_list_t head;
+	play_node_t *pos;
+	Pagination_t paging;
+
+	List_Init(head, play_node_t);
+	paging.pageSize = 0;///不知道该等于几 
+
+	//载入数据
+	paging.totalRecords = Play_Srv_FetchAll(head);
+	Paging_Locate_FirstPage(head, paging);
+	do {
+		
+		
+		printf(
+				"******************************************************************\n");
+		printf(
+				"[P]revPage | [N]extPage | [A]dd | [D]elete | [U]pdate | [S]howlist | [R]eturn");
+		printf(
+				"\n==================================================================\n");
+		printf("Your Choice:");
+		fflush(stdin);
+		scanf("%c", &choice);
+		fflush(stdin);
+
+		switch (choice) {
+		case 'a':
+		case 'A':
+			if (Play_UI_Add()) //新添加成功，跳到最后一页显示
+			{
+				paging.totalRecords = Studio_Srv_FetchAll(head);
+				Paging_Locate_LastPage(head, paging, play_node_t);
+			}
+			break;
+		case 'd':
+		case 'D':
+			printf("Input the ID:");
+			scanf("%d", &id);
+			if (play_UI_Delete(id)) {	//从新载入数据
+				paging.totalRecords = Play_Srv_FetchAll(head);
+				List_Paging(head, paging, play_node_t);
+			}
+			break;
+		case 'u':
+		case 'U':
+			printf("Input the ID:");
+			scanf("%d", &id);
+			if (Play_UI_Modify(id)) {	//从新载入数据
+				paging.totalRecords = Play_Srv_FetchAll(head);
+				List_Paging(head, paging, play_node_t);
+			}
+			break;
+		case 's':
+		case 'S'://////有问题 
+			Play_UI_ShoeList(play_list_t list,Pagination_t paging) 
+			paging.totalRecords = Play_Srv_FetchAll(head);
+			List_Paging(head, paging, play_node_t);
+			break;
+		case 'p':
+		case 'P':
+			if (1 < Pageing_CurPage(paging)) {
+				Paging_Locate_OffsetPage(head, paging, -1, play_node_t);
+			}
+			break;
+		case 'n':
+		case 'N':
+			if (Pageing_TotalPages(paging) > Pageing_CurPage(paging)) {
+				Paging_Locate_OffsetPage(head, paging, 1, play_node_t);
+			}
+			break;
+		}
+	} while (choice != 'r' && choice != 'R');
+	//释放链表空间
+	List_Destroy(head, play_node_t);
 
 }
 
@@ -53,7 +136,50 @@ void Play_UI_MgtEntry(int flag){
  */
 int Play_UI_Add(void)
 {
-	return 0;
+	play_t rec;
+//	int newRecCount = 0;
+	char choice;
+
+	do {
+		/*system("cls");*/
+		printf("\n=======================================================\n");
+		printf("****************  添加新的剧目 ****************\n");
+		printf("-------------------------------------------------------\n");
+		printf("剧目名称:");
+		fflush(stdin);
+		gets(rec.name);
+		printf("\n");
+		printf("剧目类型:");
+		scanf();//不会 
+		printf("出品地区：");
+		scanf("%d\n", rec.area);
+		printf("剧目等级：");
+		//不会
+		printf("时长:");
+		scanf("%d\n",rec.duration);
+		printf("开始日期：");
+		//不会 
+		printf("结束日期：");
+		//不会
+		printf("票价：");
+		scanf("%d\n",rec.price);
+		 
+		printf("=======================================================\n");
+
+		//获取主键
+		rec.id = EntKey_Srv_CompNewKey("Play");
+
+		if (Play_Srv_Add(&rec)) {
+			newRecCount += 1;
+			printf("添加成功!\n");
+		} else
+			printf("添加失败!\n");
+		printf("-------------------------------------------------------\n");
+		printf("[A]dd more, [R]eturn:");
+		fflush(stdin);
+		scanf("%c", &choice);
+	} while ('a' == choice || 'A' == choice);
+	return newRecCount;
 }
 
 /*
